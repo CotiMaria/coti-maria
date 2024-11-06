3616be49df9ed890a3aa541a80e905f5b6e06918
CotiMaria <Coti.Mi.Maria@student.utcluj.ro>
2024-11-06 14:00:55 +0200

    tema 4.1

---
+++ tema 4.1.py
@@ -0,0 +1,29 @@
+# 1. Lista de cuvinte și alegerea cuvantului
+import random
+cuvinte = ["python", "programare", "calculator", "date", "algoritm"]
+cuvant_de_ghicit = random.choice(cuvinte)
+progres = ["_" for _ in cuvant_de_ghicit]
+
+# 2. Am scris cate incercari sunt
+incercari_ramase = 6
+litere_incercate = []
+print(" ".join(progres))
+while incercari_ramase > 0 and "_" in progres:
+    litera = input("Introdu o literă: ").lower()
+    if len(litera) != 1 or not litera.isalpha():
+        print("Te rog să introduci o singură literă.")
+        continue
+    if litera in litere_incercate:
+        print("Ai încercat deja litera aceasta.")
+        continue
+    litere_incercate.append(litera)
+    if litera in cuvant_de_ghicit:
+        for i, char in enumerate(cuvant_de_ghicit):
+            if char == litera:
+                progres[i] = litera
+        print("Bravo! Litera se află în cuvânt.")
+    else:
+        incercari_ramase -= 1
+        print("Litera nu se află în cuvânt. Încercări rămase:", incercari_ramase)
+    print("Progres:", " ".join(progres))
+    print("Încercări rămase:", incercari_ramase)
